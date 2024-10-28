738f3dce3712d083ac9150bb1080f39062f342a8
Ubuntu <ubuntu@ip-172-31-45-139.ap-northeast-2.compute.internal>
2024-10-28 00:45:59 +0000

    feat: 압축해제 방식 변경, 도커파일 빌드명 변경

--- deploy/deploy_back.sh
+++ deploy/deploy_back.sh
@@ -6,7 +6,7 @@ fi
 
 # tar 파일 다운로드
 BUILD_PATH="docker/data/server"
-aws s3 cp s3://soundflyer-bucket/dev-deploy/$1.tar.gz $BUILD_PATH/$1.tar.gz
+aws s3 cp s3://soundflyer/soundflyer-deploy/$1.tar.gz $BUILD_PATH/$1.tar.gz
 
 # 기존 Jar 삭제
 if [ -d $BUILD_PATH/*.jar ]
@@ -14,17 +14,18 @@ then
     rm -r *.jar
 fi
 
-# jar 추출
-tar -xvf $BUILD_PATH/$1.tar -C $BUILD_PATH --wildcards '*.jar'
+# 압축 해제
+tar -zxf $BUILD_PATH/$1.tar.gz -C $BUILD_PATH
+rm $BUILD_PATH/$1.tar.gz
 
 #기존 컨테이너 삭제
-docker rm -f soundflyer-back
+docker rm -f soundflyer-back:$2
 
 # dockerfile 빌드
-docker build --no-cache -t soundflyer-back:$2 -f docker/soundflyer-back.dockerfile .
+docker build --no-cache -t soundflyer-back:$2 -f docker/back.Dockerfile .
 
 # docker-compose 실행
 docker-compose up -d soundflyer-back
 
 ### Cleanup
-docker rmi $(docker images --filter "dangling=true" -q)
+#docker rmi $(docker images --filter "dangling=true" -q)
