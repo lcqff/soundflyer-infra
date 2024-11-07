0e0a362643ff30f5da3e7c2611b015948e4ac83b
Ubuntu <ubuntu@ip-172-31-45-139.ap-northeast-2.compute.internal>
2024-11-07 02:48:41 +0000

    fix: S3 파일 다운로드시 와일드카드를 삭제한다.

--- deploy/deploy_front.sh
+++ deploy/deploy_front.sh
@@ -12,8 +12,7 @@ fi
 rm -r $BUILD_PATH/*
 
 ### Download Build Files From S3
-BUILD_PATH="docker/data/server"
-aws s3 cp s3://soundflyer/soundflyer-deploy/front/$1.tar.gz $BUILD_PATH/$1.tar.gz
+aws s3 cp s3://soundflyer/soundflyer-deploy/front/ $BUILD_PATH --include=$1.tar.gz* --recursive
 
 ### Unzip Build Files
 cd $BUILD_PATH
